2872a9c2ccf554594328c56ba1f76578cfaeca2d
AntonioGiangrave <tony@bslt.it>
2016-12-20 12:26:04 +0100

    Init
    Già installato debugbar, laravel collective, make:auth

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.less('app.less');
+    mix.sass('bootstrap.scss');
+
+
+    mix.copy('bower_components/bootstrap/dist/fonts', 'httpdocs/assets/fonts');
+    mix.copy('bower_components/font-awesome/fonts', 'httpdocs/assets/fonts');
+    mix.styles([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/fontawesome/css/font-awesome.css',
+        'resources/css/sb-admin-2.css',
+        'resources/css/timeline.css',
+
+        'resources/css/general.css',
+        'resources/css/calendar.css',
+
+        'resources/css/animate.css',
+        'resources/css/icomoon.css',
+
+        'resources/css/owl.carousel.min.css',
+        'resources/css/owl.theme.default.min.css',
+        'resources/css/style.css',
+
+        //DATE PICKER
+        'resources/css/jquery-ui.css',
+
+
+
+    ], 'httpdocs/assets/stylesheets/styles.css', './');
+    mix.scripts([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        // 'bower_components/Chart.js/Chart.js',
+        'bower_components/metisMenu/dist/metisMenu.js',
+        'resources/js/sb-admin-2.js',
+        //'resources/js/frontend.js',
+
+        'resources/js/main.js',
+        // 'resources/js/modernizr-2.6.2.min.js',
+        'resources/js/modernizr.js',
+        'resources/js/owl.carousel.min.js',
+        'resources/js/respond.min.js',
+        'resources/js/jquery.easing.1.3.js',
+        'resources/js/listgroup.js',
+
+
+        //DATE PICKER
+        'resources/js/jquery-ui.js',
+
+
+
+        // 'resources/js/bootstrap.js',
+
+
+    ], 'httpdocs/assets/scripts/frontend.js', './');
+});
---
+++ resources/js/main.js
@@ -0,0 +1,230 @@
+;(function () {
+
+	'use strict';
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+		return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
+			(navigator.platform.indexOf("iPod") != -1)
+		);
+	};
+
+	var dropDownMenu = function() {
+
+		$(".drop-down-menu").hover(function(){
+				$(this).addClass('active');
+			},
+			function(){
+				$(this).removeClass('active');
+			});
+
+	};
+
+	var loaderPage = function() {
+
+		$(".fh5co-loader").fadeOut("slow");
+	};
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+
+	};
+
+
+	var owlCarouselMain = function() {
+		var owl = $('.owl-carousel-main');
+
+		owl.owlCarousel({
+			items:1,
+			mouseDrag: true,
+			loop: true,
+			margin: 0,
+			responsiveClass: true,
+			nav: true,
+			dots: true,
+			autoHeight: true,
+			smartSpeed: 500,
+			autoplay: true,
+			autoplayHoverPause: true,
+			navText: [
+				"<i class='icon-chevron-left owl-direction'></i>",
+				"<i class='icon-chevron-right owl-direction'></i>"
+			]
+		});
+
+		$(window).resize(function(){
+			owl.trigger('refresh.owl.carousel');
+		});
+	};
+
+	var offcanvasMenu = function() {
+		$('#wrap').prepend('<div id="fh5co-offcanvas" />');
+		$('#fh5co-offcanvas').append( $('#fh5co-main-nav').clone() );
+	};
+
+
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+			var $this = $(this);
+
+			if ( $('body').hasClass('offcanvas-visible') )  {
+				$('body').removeClass('fh5co-overflow offcanvas-visible');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('fh5co-overflow offcanvas-visible');
+				$this.addClass('active');
+			}
+
+			event.preventDefault();
+
+		});
+
+		$('#fh5co-offcanvas').css('height', $(window).height());
+
+		$(window).resize(function() {
+			$('#fh5co-offcanvas').css('height', $(window).height());
+			if ( $('body').hasClass('offcanvas-visible') ) {
+				$('body').removeClass('offcanvas-visible');
+				$('.js-fh5co-nav-toggle').removeClass('active');
+			}
+		});
+
+		$(window).scroll(function(){
+			if ( $('body').hasClass('offcanvas-visible') ) {
+				$('body').removeClass('offcanvas-visible');
+				$('.js-fh5co-nav-toggle').removeClass('active');
+			}
+		});
+
+	};
+
+
+	/**********************************************************************************************************************
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 FUNZIONTI TONY
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 * **********************************************************************************************************************
+	 ***********************************************************************************************************************/
+
+
+	//fade messaggio conferma
+	$('.alert-success').delay(5000).fadeOut('slow');
+
+
+
+	$( document ).ready(function() {
+		//filtri su liste
+		$("#resetfilter").click(function () {
+			$("#txtSearch").val('');
+			var search = $('#txtSearch').val().toLowerCase();
+			$(".list-group-item").each(function () {
+				if ($(this).html().toLowerCase().indexOf(search) != -1) {
+					$(this).show();
+				}
+				else {
+					$(this).hide();
+				}
+			});
+		});
+
+		//filtri su liste
+		$("#soloselezionati").click(function () {
+			$("#txtSearch").val('');
+			var search = $('#txtSearch').val().toLowerCase();
+			$(".list-group-item").each(function () {
+				if ($(this).hasClass('active')) {
+					$(this).show();
+				}
+				else {
+					$(this).hide();
+				}
+			});
+		});
+
+		//filtri su liste
+		$("#txtSearch").on('keyup', function () {
+			var search = $(this).val().toLowerCase();
+			$(".list-group-item").each(function () {
+				if ($(this).html().toLowerCase().indexOf(search) != -1) {
+					$(this).show();
+				}
+				else {
+					$(this).hide();
+				}
+			});
+		});
+
+
+
+
+	});
+
+
+
+
+
+
+	/**********************************************************************************************************************
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 //FINE  FUNZIONTI TONY
+	 **********************************************************************************************************************
+	 **********************************************************************************************************************
+	 * **********************************************************************************************************************
+	 ***********************************************************************************************************************/
+
+
+
+
+
+// Document on load.
+	$(function(){
+
+		dropDownMenu();
+		owlCarouselMain();
+		offcanvasMenu();
+		burgerMenu();
+		loaderPage();
+		goToTop();
+
+	});
+
+
+}());
+
+//espandi tabella corsi
+function showDetailCorso(id){
+	$('.dettagliocorso').addClass('hide');
+	$('#corso'+id).removeClass('hide');
+}
